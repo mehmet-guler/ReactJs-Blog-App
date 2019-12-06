35cf96f5473b084942dc06d5747c69a8ddb8e1ba
mehmet-guler <33619658+mehmet-guler@users.noreply.github.com>
2019-12-07 01:43:59 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostList from './PostList';
+
+const App = () => {
+    console.log("App")
+    return (
+        <div className="ui container">
+            <PostList />
+        </div>
+    )
+}
+
+export default App;
